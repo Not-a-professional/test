f702b358ed2e41dee5df4bf8a90e9ab014bc0c85
liwei <805717605@qq.com>
2018-07-09 15:13:56 +0800

    hibernate jdbc增删改查

---
+++ src/Main.java
@@ -0,0 +1,125 @@
+import java.sql.Connection;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        jdbc j = new jdbc();
+        Connection connection = j.getConnection();
+    }
+
+    public void test1() {
+        Scanner scanner = new Scanner(System.in);
+        while(scanner.hasNext()) {
+            Long num = scanner.nextLong();
+            if (num.toString().length() > 12) {
+                System.out.print("输入长度不能超过12位\n");
+            } else {
+                if (num.toString().length() <= 4) {
+                    result(num, 1, true, true);
+                } else {
+                    result(num, 1, false, false);
+                }
+                System.out.print("\n");
+            }
+        }
+    }
+
+    private static void result(Long a, int depth, boolean start, boolean four) {
+        if (a.toString().length() > 4 && !start) {
+            if (Long.valueOf(a / 10000).toString().length() <= 4) {
+                result(a / 10000, depth + 1, true, four);
+            } else {
+                result(a / 10000, depth + 1, false, four);
+            }
+        } else if(a.toString().length() <= 4 && !start) {
+            result(a, depth + 1, true, four);
+        }
+        long temp = a % 10000;
+        if (temp != 0) {
+            if (depth == 1 && a.toString().length() > 8
+                    && a / 10000 % 10000 == 0 && (temp / 1000) != 0) {
+                System.out.print('零');
+            }
+            boolean flag = false;
+            for (int i = 3; i >= 0; --i) {
+                long t = temp / power(i);
+                if (t == 0) {
+                    if (i == 3) {
+                        if (!start) {
+                            System.out.print('零');
+                            flag = !flag;
+                        }
+                    }
+                    if ((!flag && i != 3 && !start) || (four && flag)) {
+                        System.out.print('零');
+                        flag = !flag;
+                    }
+                } else {
+                    System.out.print(getChar(t));
+                    if (i != 0) {
+                        System.out.print(getDan(i));
+                    }
+                    flag = !flag;
+                }
+                temp = temp % power(i);
+                if (temp == 0) {
+                    break;
+                }
+            }
+            if (depth == 2) {
+                System.out.print('万');
+            } else if (depth == 3) {
+                System.out.print('亿');
+            }
+        } else if (start) {
+            System.out.print('零');
+        }
+    }
+
+    private static long power(int i) {
+        long temp = 1;
+        for (;i > 0; --i) {
+            temp *= 10;
+        }
+        return temp;
+    }
+
+    private static char getChar(long i) {
+        if (i == 0) {
+            return '零';
+        } else if (i == 1) {
+            return '一';
+        } else if (i == 2) {
+            return '二';
+        } else if (i == 3) {
+            return '三';
+        } else if (i == 4) {
+            return '四';
+        } else if (i == 5) {
+            return '五';
+        } else if (i == 6) {
+            return '六';
+        } else if (i == 7) {
+            return '七';
+        } else if (i == 8) {
+            return '八';
+        } else if (i == 9) {
+            return '九';
+        } else {
+            return ' ';
+        }
+    }
+
+    private static char getDan(int i) {
+        if (i == 1) {
+            return '十';
+        } else if (i == 2) {
+            return '百';
+        } else if (i == 3) {
+            return '千';
+        } else {
+            return ' ';
+        }
+    }
+}
---
+++ src/jdbc.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+class jdbc {
+    public Connection getConnection() {
+        String url="jdbc:mysql://localhost:3306/user";
+        String username="root";
+        String password="wwwliwei001com";
+        String driver = "com.mysql.jdbc.Driver";
+        Connection conn = null;
+        try {
+            Class.forName(driver); //classLoader,加载对应驱动
+            conn = (Connection) DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+            e.getLocalizedMessage();
+        } catch (SQLException e) {
+            e.getLocalizedMessage();
+        }
+        System.out.println("系统启动！");
+        return conn;
+    }
+}
